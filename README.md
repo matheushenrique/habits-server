888a631230792708b618eb3d5267d0b927d27819
matheushenrique <matheus.henriquepr@gmail.com>
2023-01-31 14:44:11 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+usefull commands
+  npx tsc --init ( create tsconfig file, change target to es2020)
+  npm i tsx -D (enable execute ts files)
+  fastify (similar to express)
+  npx prisma init --datasource-provider SQLite
+  npx prisma migrate dev (check all changes made in the schama and create a migration file)
+  npx prisma studio (open prisma on browser)
+  using vitejs.dev
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany({where: {
+    title: {
+      startsWith: 'test'
+    }
+  }})
+  return habits
+})
+
+app.listen({port: 3333})
